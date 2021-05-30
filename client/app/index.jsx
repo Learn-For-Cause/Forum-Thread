f4cddb5601cb0cb3fc1462fe858c14b0e56b12f2
vipinRohilla <vipinrohilla270898@gmail.com>
2021-05-30 14:27:01 +0530

    nothing change

--- client/app/index.jsx
+++ client/app/index.jsx
@@ -4,7 +4,7 @@ import { render } from "react-dom";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import App from "./components/App/App";
 import NotFound from "./components/App/NotFound";
-import Home from "./components/Home/Home";
+// import Home from "./components/Home/Home";
 import HelloWorld from "./components/HelloWorld/HelloWorld";
 import BelowHeader from "../app/components/BelowHeader/BelowHeader"
 import "./styles/styles.scss";
